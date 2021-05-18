15a4f86c6b7181bb01ef83793e47eaae8be3e66a
suchi29 <root@p-k21iecpyfy4j-workstation.c.instruqt-prod.internal>
2021-05-18 15:11:29 +0000

    Added vnet module

--- vnet.tf
+++ vnet.tf
@@ -1,5 +1,7 @@
 module "network" {
   source  = "app.terraform.io/suchithra-training/network/azurerm"
   version = "3.0.1"
+
+  resource_group_name       = azurerm_resource_group.myresourcegroup.name
   # insert required variables here
 }
